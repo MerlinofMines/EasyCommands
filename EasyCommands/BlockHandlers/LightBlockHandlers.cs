702f18fe5bb060bcfd42fc92607f496d38a0420f
pandincus <daniel.pincas@gmail.com>
2021-03-24 00:00:27 -0700

    Added support for tweaking light settings
    Added support for tweaking light settings
    Includes falloff, intensity, blink interval, blink length,
    and blink offset
    
    This commit reveals to me how finding a more generic solution for
    editing block properties (without having to add code) would be
    preferable, but as this was my first real attempt at following the
    existing patterns to add new settings, I wanted to mostly follow
    the existing mold. I might explore something more generic
    next time I'm adding property support.
    
    (Also, I just really wanted to see it working!)
    
    This fixes #10

--- EasyCommands/BlockHandlers/LightBlockHandlers.cs
+++ EasyCommands/BlockHandlers/LightBlockHandlers.cs
@@ -23,6 +23,11 @@ namespace IngameScript {
             public LightBlockHandler() {
                 AddStringHandler(PropertyType.COLOR, (b) => b.Color.ToString(), (b, v) => b.Color = GetColor(v));
                 AddNumericHandler(PropertyType.RANGE, (b) => b.Radius, (b, v) => b.Radius = v, 3);
+                AddNumericHandler(PropertyType.BLINK_INTERVAL, (b) => b.BlinkIntervalSeconds, (b, v) => b.BlinkIntervalSeconds = v, 0.1f);
+                AddNumericHandler(PropertyType.BLINK_LENGTH, (b) => b.BlinkLength, (b, v) => b.BlinkLength = v, 0.1f);
+                AddNumericHandler(PropertyType.BLINK_OFFSET, (b) => b.BlinkOffset, (b, v) => b.BlinkOffset = v, 0.1f);
+                AddNumericHandler(PropertyType.INTENSITY, (b) => b.Intensity, (b, v) => b.Intensity = v, 1f);
+                AddNumericHandler(PropertyType.FALLOFF, (b) => b.Falloff, (b, v) => b.Falloff = v, 0.5f);
                 defaultPropertiesByPrimitive[PrimitiveType.STRING] = PropertyType.COLOR;
                 defaultPropertiesByPrimitive[PrimitiveType.NUMERIC] = PropertyType.RANGE;
                 defaultPropertiesByDirection.Add(DirectionType.UP, PropertyType.RANGE);
