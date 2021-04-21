8b4e8d3022dc374a88716dc85f788738a0c510a2
Merlin <merlinofmines@gmail.com>
2021-04-20 20:20:19 -0600

    Added line number to exception output when unable to parse
    This commit adjusts the parsing to include the line number in the exception output when the command cannot be parsed, which drastically reduces the time spent
    searching for the culprit in a script that isn't working.
    
    As part of this commit, the parsing is changed to not pre-emptively ignore comments and empty space.  Instead, these entries are ignored while parsing the commands
    so that the correct line numbers remain intact

---
+++ EasyCommands.Tests/ScriptTests/SimpleLoggingTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using IngameScript;
+
+namespace EasyCommands.Tests.ScriptTests {
+    [TestClass]
+    public class SimpleLoggingTests {
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParse() {
+            String script = @"
+:main
+print 'Hello World'
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 3"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseAndImpliedMain() {
+            String script = @"
+print 'Hello World'
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 2"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseNestedCommand() {
+            String script = @"
+if (true)
+  print 'Hello World'
+  'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 3"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseAfterNestedCommand() {
+            String script = @"
+if (true)
+  print 'Hello World'
+  wait 5
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 4"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseIncludesComments() {
+            String script = @"
+if (true)
+  print 'Hello World'
+  wait 5
+#This line isn't working
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 5"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseIncludesEmptySpace() {
+            String script = @"
+:main
+if (true)
+  print 'Hello World'
+  wait 5
+
+:broken
+#This line isn't working
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 8"));
+            }
+        }
+
+        [TestMethod]
+        public void PrintCorrectLineNumberWhenUnableToParseInsideFunction() {
+            String script = @"
+:main
+if (true)
+  print 'Hello World'
+  wait 5
+
+:broken
+'Hello World'
+";
+            using (var test = new ScriptTest(script)) {
+                Program.LOG_LEVEL = Program.LogLevel.INFO;
+                test.RunOnce();
+
+                Assert.IsTrue(test.Logger.Contains("Unable to parse command from command parameters at line number: 7"));
+            }
+        }
+    }
+}
