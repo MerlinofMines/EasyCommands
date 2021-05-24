fa760d561457f0e8f4ed4d02288db43190948dd6
Merlin <merlinofmines@gmail.com>
2021-05-23 23:07:01 -0600

    Poison Pill To Test Tests

---
+++ EasyCommands.Tests/ScriptTests/FailingTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EasyCommands.Tests.ScriptTests {
+    [TestClass]
+    public class FailingTest {
+        [TestMethod]
+        public void AlwaysFail() {
+            Assert.IsTrue(false);
+        }
+    }
+}
