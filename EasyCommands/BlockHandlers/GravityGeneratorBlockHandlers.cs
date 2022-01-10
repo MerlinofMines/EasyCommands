efc3676cbd30c7f96d6398d20bd0e1d40e1412c7
Merlin <merlinofmines@gmail.com>
2022-01-09 21:20:16 -0700

    Gravity Generator Block Improvements (#137)
    This commit adds the Level property to Gravity and Spherical Gravity Generator Blocks.  Also added a missing default property for gravity spheres.

--- EasyCommands/BlockHandlers/GravityGeneratorBlockHandlers.cs
+++ EasyCommands/BlockHandlers/GravityGeneratorBlockHandlers.cs
@@ -21,15 +21,19 @@ namespace IngameScript {
     partial class Program {
         public class SphericalGravityGeneratorBlockHandler : FunctionalBlockHandler<IMyGravityGeneratorSphere> {
             public SphericalGravityGeneratorBlockHandler() {
+                var rangeHandler = NumericHandler(b => b.Radius, (b, v) => b.Radius = v, 25);
+                AddPropertyHandler(Property.RANGE, rangeHandler);
+                AddPropertyHandler(Property.LEVEL, rangeHandler);
                 AddNumericHandler(Property.STRENGTH, b => b.GravityAcceleration, (b,v) => b.GravityAcceleration = v, 0.25f);
-                AddNumericHandler(Property.RANGE, b => b.Radius, (b, v) => b.Radius = v, 25);
+
+                defaultPropertiesByPrimitive[Return.NUMERIC] = Property.STRENGTH;
             }
         }
 
         public class GravityGeneratorBlockHandler : FunctionalBlockHandler<IMyGravityGenerator> {
             public GravityGeneratorBlockHandler() {
                 AddNumericHandler(Property.STRENGTH, b => b.GravityAcceleration, (b, v) => b.GravityAcceleration = v, 0.25f);
-                AddDirectionHandlers(Property.RANGE, Direction.NONE,
+                var rangeHandler = DirectionalTypedHandler(Direction.NONE,
                     TypeHandler(ReturnTypedHandler(Return.VECTOR,
                         TypeHandler(VectorHandler(b => b.FieldSize, (b, v) => b.FieldSize = v), Return.VECTOR),
                         TypeHandler(NumericHandler(b => b.FieldSize.Length(), (b, v) => b.FieldSize = new Vector3(v, v, v), 25), Return.NUMERIC)), Direction.NONE),
@@ -37,6 +41,8 @@ namespace IngameScript {
                     TypeHandler(NumericHandler(b => b.FieldSize.X, (b, v) => b.FieldSize = new Vector3(v, b.FieldSize.Y, b.FieldSize.Z)), Direction.LEFT, Direction.RIGHT),
                     TypeHandler(NumericHandler(b => b.FieldSize.Z, (b, v) => b.FieldSize = new Vector3(b.FieldSize.X, b.FieldSize.Y, v)), Direction.FORWARD, Direction.BACKWARD)
                     );
+                AddPropertyHandler(Property.RANGE, rangeHandler);
+                AddPropertyHandler(Property.LEVEL, rangeHandler);
 
                 defaultPropertiesByPrimitive[Return.NUMERIC] = Property.STRENGTH;
                 defaultPropertiesByPrimitive[Return.VECTOR] = Property.RANGE;
